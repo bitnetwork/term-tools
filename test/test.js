9c9b7315c172e9e46fc48b69c9b3a14493590283
Bit <bitnetwork@users.noreply.github.com>
2017-11-07 22:06:12 -0700

    Clean up for incoming terminfo

--- test/test.js
+++ test/test.js
@@ -6,12 +6,15 @@ function main() {
   terminal.changeScreen(true);
   setTimeout(function() {
     terminal.clear();
+    try{terminal.statusLine("OH LOOK A TITLE");} catch(e) {}
     terminal.write("Look at this terminal that I just found.\nLEGIT THO LOOK AT ALL DIS FREE SPACE TO DO SHIT\nBest of all, when this exits, it all gets erased and it looks like nothing happened.\n(p.s.) don't do ctrl-c on this screen cause you can't get back to your old scrollback in the terminal\n(p.s.s.) your cursor is gone via magic");
-    terminal.cursorVisible(false);
+    terminal.reset();
+    terminal.write("test");
+    try {terminal.cursorVisible(false);} catch(e) {}
     
     setTimeout(function() {
       terminal.changeScreen(false);
-      terminal.cursorVisible(true);
+      try {terminal.cursorVisible(true);} catch(e) {}
       terminal.write("And here we are back at the shell again.\nHow cooul is dat?\nWell k bai lol\n");
     }, 7000);
   }, 1000);
