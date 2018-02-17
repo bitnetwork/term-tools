ffca884346b40ff55dfafa388237261bb931812e
Bit <bitnetwork@users.noreply.github.com>
2018-02-17 00:13:15 -0700

    Terminfo parameterized string should be done now, new regex, flags, width & percision done

--- test/terminfo-parsing.js
+++ test/terminfo-parsing.js
@@ -12,6 +12,6 @@ if (terminfo.format(terminfo.capabilities.CURSOR_ADDRESS, 0, 0) === "\u001b[1;1H
 
 // CHANGE_SCROLL_REGION "\u001b[%i%p1%d;%p2%dr"
 //                                          10 v      20 v      30 v      40 v
-process.stdout.write(terminfo.format(" begin%? statement %p1%t %p2%p3%/%d %efalse%; end ", parseInt(process.argv[2]), parseInt(process.argv[3])));
-
+process.stdout.write(terminfo.format("|%'a'%s|" /*"b%? s%p1%p2%A%d%t true%e false%;"*/ /*" begin%? statement %p1%t %p2%p3%+%d %e %p2%p3%*%d %; end "*/, parseInt(process.argv[2]), process.argv[3], parseInt(process.argv[4])));
+console.log(`\n${process.argv}`)
 //process.stdout.write(termin);
