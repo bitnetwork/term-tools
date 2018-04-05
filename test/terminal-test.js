b75053bfae47fff05b404150902213b760ba80b7
Bit <bitnetwork@users.noreply.github.com>
2018-04-04 22:01:58 -0600

    key event working & firing

--- test/terminal-test.js
+++ test/terminal-test.js
@@ -2,7 +2,24 @@ const Terminal = require("..").Terminal;
 
 let term = new Terminal();
 
-term.open();
-term.write(`${term.changeScreen()}Testing terminal module...\n\
-${term.cursorVisible(false)}Cursor now invisible.\n\
-${term.moveRelative(0, -5)}Now moved 5 lines up.`);
+// term.write(`${term.changeScreen()}Testing terminal module...\n\
+// ${term.cursorVisible(false)}Cursor now invisible.\n\
+// ${term.moveRelative(0, -5)}Now moved 5 lines up.`);
+
+if (process.argv.length <= 2) {
+  term.open();
+}
+
+term.on("key", function(value, mapping) {
+  let output = "";
+  for (let i = 0; i < value.length; i++) {
+    output += value.charCodeAt(i) + (i < value.length - 1 ? ", " : "");
+  }
+  
+  console.log(`caught: ${mapping === null ? value : mapping} [${output}]`);
+
+  if (value == term.KEY_CTRL_C) {
+    term.close();
+    process.exit(0);
+  }
+});
