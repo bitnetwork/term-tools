417eb95dc3a7a5db22e9a08586323df9f729f228
Bit <bitnetwork@users.noreply.github.com>
2018-04-29 22:32:09 -0600

    Full color support, attribute support, bell support, terminfo now defines all capabilites, new testing file with terminal list to support

--- test/terminal-test.js
+++ test/terminal-test.js
@@ -1,24 +1,7 @@
 const Terminal = require("..").Terminal;
 
-let term = new Terminal({terminal: process.argv[2]});
-
-console.log(process.argv[2]);
-// term.write(`${term.changeScreen()}Testing terminal module...\n\
-// ${term.cursorVisible(false)}Cursor now invisible.\n\
-// ${term.moveRelative(0, -5)}Now moved 5 lines up.`);
+let term = new Terminal({terminal: process.argv[3] || process.env.TERM || "xterm"});
 
 term.open();
 
-term.on("key", function(value, mapping) {
-  let output = "";
-  for (let i = 0; i < value.length; i++) {
-    output += value.charCodeAt(i) + (i < value.length - 1 ? ", " : "");
-  }
-  
-  console.log(`caught: ${mapping === null ? value : mapping} [${output}]`);
-
-  if (value == term.KEY_CTRL_C) {
-    term.close();
-    process.exit(0);
-  }
-});
+console.log(term);
